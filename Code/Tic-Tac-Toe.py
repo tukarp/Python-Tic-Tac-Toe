c46361a03a9e05a562eff074a3730957ff3cb410
tukarp <114019481+tukarp@users.noreply.github.com>
2023-06-26 14:51:09 +0200

    Done

--- Code/Tic-Tac-Toe.py
+++ Code/Tic-Tac-Toe.py
@@ -1,5 +1,15 @@
-# Simple Tic-Tac-Toe Game
+# My Tic-Tac-Toe Game
 # Made by github.com/tukarp
+
+
+# Initialize logo
+LOGO = """
+  _______          ______               ______         
+ /_  __(_)____    /_  __/___ ______    /_  __/___  ___ 
+  / / / / ___/_____/ / / __ `/ ___/_____/ / / __ \/ _ \\
+ / / / / /__/_____/ / / /_/ / /__/_____/ / / /_/ /  __/
+/_/ /_/\___/     /_/  \__,_/\___/     /_/  \____/\___/ 
+"""
 
 
 # Move class
@@ -21,14 +31,14 @@ class Player(object):
     # Get player move
     def get_move(self):
         # Choose coordinates message
-        print("Choose coordinates ([0-2] [0-2]):")
+        print("│ Choose coordinates ([0-2] [0-2]) ")
 
         # Get correct input from player loop
         while True:
             # Try block
             try:
                 # Get row and column from input
-                row, column = input(f"{self.name}: ").split()
+                row, column = input(f"│ {self.name}: ").split()
 
                 # Check if row and column are digits
                 if row.isdigit() and column.isdigit():
@@ -37,15 +47,15 @@ class Player(object):
                         # Return move
                         return Move(int(row), int(column), self.sign)
                     else:
-                        # Print error message
-                        print("Wrong coordinates!")
+                        # Print "Wrong coordinates!" error message
+                        print("│ Wrong coordinates!")
                 else:
-                    # Print error message
-                    print("You should enter numbers!")
+                    # Print "You should enter numbers!" error message
+                    print("│ You should enter numbers!")
             # Raise ValueError exception
             except ValueError:
-                # Print error message
-                print("Value Error!")
+                # Print "Value Error!" error message
+                print("│ Value Error!")
 
 
 # Grid class
@@ -53,7 +63,7 @@ class Grid(object):
     # Define constructor
     def __init__(self):
         # Create grid
-        self.grid = [["█", "█", "█"] for i in range(3)]
+        self.grid = [["■", "■", "■"] for i in range(3)]
 
     # Print grid
     def __str__(self):
@@ -73,16 +83,16 @@ class Grid(object):
         return state
 
     # Get grid field
-    def get_field(self, row, y):
+    def get_field(self, row, column):
         # Return field in grid
-        return self.grid[row][y]
+        return self.grid[row][column]
 
     # Set grid field
     def set_field(self, move):
         # Check if field is empty
-        if self.get_field(move.row, move.column) != "█":
-            # Print error message
-            print("This field is taken!")
+        if self.get_field(move.row, move.column) != "■":
+            # Print "This field is taken!" error message
+            print("│ This field is taken!")
             # Return False
             return False
         # Set field
@@ -92,14 +102,14 @@ class Grid(object):
 
     # Print grid
     def print_grid(self):
-        print(f"""
-          ◪ X 0 1 2
-          Y ─────────
-          0 │ {self.grid[0][0]} {self.grid[0][1]} {self.grid[0][2]} │
-          1 │ {self.grid[1][0]} {self.grid[1][1]} {self.grid[1][2]} │
-          2 │ {self.grid[2][0]} {self.grid[2][1]} {self.grid[2][2]} │
-            ────────
-            """)
+        print(f"""[======================================================]
+│~~~~~~~~~~~~~~~~~~~~~ ◪ X 0 1 2   ~~~~~~~~~~~~~~~~~~~~│
+│===================== Y ───────── ====================│
+│~~~~~~~~~~~~~~~~~~~~~ 0 │ {self.grid[0][0]} {self.grid[0][1]} {self.grid[0][2]} │ ~~~~~~~~~~~~~~~~~~~~│
+│~~~~~~~~~~~~~~~~~~~~~ 1 │ {self.grid[1][0]} {self.grid[1][1]} {self.grid[1][2]} │ ~~~~~~~~~~~~~~~~~~~~│
+│===================== 2 │ {self.grid[2][0]} {self.grid[2][1]} {self.grid[2][2]} │ ====================│
+│~~~~~~~~~~~~~~~~~~~~~   ────────  ~~~~~~~~~~~~~~~~~~~~│
+[======================================================]""")
 
 
 # Game class
@@ -116,8 +126,8 @@ class Game(object):
 
     # Play game
     def play(self, player_one, player_two):
-        # Print welcome message
-        print(f"Welcome to {self.name} Game!")
+        # Print logo
+        print(LOGO)
         # Create players tuple
         players = (player_one, player_two)
         # Initialize current player
@@ -134,68 +144,68 @@ class Game(object):
                 pass
             # Switch current player
             current_player ^= 1
-        # If winning sign is None
+        # Check if winning sign is None
         if self.winning_sign is None:
             # Print grid
             self.grid.print_grid()
             # Print draw message
-            print("Draw!")
-        # If winning sign is player one sign
+            print("│ Draw!")
+        # Check if winning sign is player one sign
         elif self.winning_sign == player_one.sign:
             # Print grid
             self.grid.print_grid()
             # Print player one won message
-            print(f"{player_one.name} ({player_one.sign}) has won!")
-        # If winning sign is player two sign
+            print(f"│ {player_one.name} ({player_one.sign}) has won!")
+        # Check if winning sign is player two sign
         elif self.winning_sign == player_two.sign:
             # Print grid
             self.grid.print_grid()
             # Print player two won message
-            print(f"{player_two.name} ({player_two.sign}) has won!")
+            print(f"│ {player_two.name} ({player_two.sign}) has won!")
 
     # Check if game is over
     def game_over(self):
         # For each row
         for row in range(3):
-            # If signs in row are the same
+            # Check if signs in row are the same
             if self.grid.get_field(row, 0) == \
                     self.grid.get_field(row, 1) == \
                     self.grid.get_field(row, 2) and \
-                    self.grid.get_field(row, 0) != '█':
+                    self.grid.get_field(row, 0) != '■':
                 # Set winning sign
                 self.winning_sign = self.grid.get_field(row, 0)
                 # Return True
                 return True
         # For each column
         for column in range(3):
-            # If signs in columns are the same
+            # Check if signs in columns are the same
             if self.grid.get_field(0, column) == \
                     self.grid.get_field(1, column) == \
                     self.grid.get_field(2, column) and \
-                    self.grid.get_field(0, column) != '█':
+                    self.grid.get_field(0, column) != '■':
                 # Set winning sign
                 self.winning_sign = self.grid.get_field(0, column)
                 # Return True
                 return True
-        # if signs on diagonal from left top are the same
+        # Check if signs on diagonal from left top are the same
         if self.grid.get_field(0, 0) == \
                 self.grid.get_field(1, 1) == \
                 self.grid.get_field(2, 2) and \
-                self.grid.get_field(0, 0) != '█':
+                self.grid.get_field(0, 0) != '■':
             # Set winning sign
             self.winning_sign = self.grid.get_field(0, 0)
             # Return True
             return True
-        # if signs on diagonal from left bottom are the same
+        # Check if signs on diagonal from left bottom are the same
         if self.grid.get_field(2, 0) == \
                 self.grid.get_field(1, 1) == \
                 self.grid.get_field(0, 2) and \
-                self.grid.get_field(2, 0) != '█':
+                self.grid.get_field(2, 0) != '■':
             # Set winning sign
             self.winning_sign = self.grid.get_field(2, 0)
             # Return True
             return True
-        # If next move is not possible
+        # Check if next move is not possible
         if not self.is_next_move_possible():
             # Set winning sign to None
             self.winning_sign = None
@@ -207,24 +217,18 @@ class Game(object):
     # Check if next move is possible
     def is_next_move_possible(self):
         # Return True if "█" is in grid state
-        return "█" in self.grid.get_state()
-
-
-# Define main function
-def main():
-    # Create game object
-    game = Game()
-
-    # Create player one
-    player_one = Player("Player One", "X")
-    # Create player two
-    player_two = Player("Player Two", "O")
-
-    # Start game with given players
-    game.play(player_one, player_two)
+        return "■" in self.grid.get_state()
 
 
 # Main function
 if __name__ == "__main__":
-    # Call main function
-    main()
+    # Create game object
+    game = Game()
+
+    # Create player one
+    player_one = Player("Player One", "x")
+    # Create player two
+    player_two = Player("Player Two", "o")
+
+    # Start game with given players
+    game.play(player_one, player_two)
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Python Tic-Tac-Toe
 
-## Simple Tic-Tac-Toe Game made in Python.
+## About
+
+### 
 
 <img src="https://github.com/tukarp/Python-Tic-Tac-Toe/blob/main/Images/Tic-Tac-Toe.png" width=500 height=500>
+
+## Gameplay
+
+
 
 ### You can play it right now under this link:
 
@@ -10,19 +16,7 @@
 https://replit.com/@tukarp/Python-Tic-Tac-Toe
 ```
 
-## Screenshots
-
-### Example Game Run (X Wins)
-
-![Example Game Run (X Wins)](https://github.com/tukarp/Python-Tic-Tac-Toe/blob/main/Images/Example%20Game%20Run%20(X%20Wins).png)
-
-### Example Game Run (O Wins)
-
-![Example Game Run (O Wins)](https://github.com/tukarp/Python-Tic-Tac-Toe/blob/main/Images/Example%20Game%20Run%20(O%20Wins).png)
-
-### Example Game Run (Draw)
-
-![Example Game Run (Draw)](https://github.com/tukarp/Python-Tic-Tac-Toe/blob/main/Images/Example%20Game%20Run%20(Draw).png)
+## Downlaod
 
 ### Requirements
 
